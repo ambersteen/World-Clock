e806534b0bbca9879ad7eb6f55871295ccc04e9f
code4mber <amberj_steen@outlook.com>
2023-12-13 14:57:34 +1100

    Added current location

--- javascript/index.js
+++ javascript/index.js
@@ -41,6 +41,9 @@ losAngelesTimeElement.innerHTML = losAngelesTime.format(
 
 function updateCity(event) {
     let cityTimezone = event.target.value;
+    if (cityTimezone === "current") {
+      cityTimezone = moment.tz.guess();
+    }
     let cityName = cityTimezone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimezone);
     let citiesElement = document.querySelector("#cities");
